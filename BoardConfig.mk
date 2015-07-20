1ea64a36b38d59588fb50502980968e1e8283781
rogersb11 <brettrogers11@gmail.com>
2015-07-20 00:49:45 -0400

    Revert "Merge branch 'android-5.1.1_r5' into HEAD"
    This reverts commit 779a5bd8519e53a37061136084e497ed58998796.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,7 +142,6 @@ BOARD_SEPOLICY_UNION += \
         mpdecision.te \
         netd.te \
         netmgrd.te \
-        wpa.te \
         platform_app.te \
         property.te \
         property_contexts \
